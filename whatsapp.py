b8f2670bd901dbe24b26fe8ec449b7ddf5444581
Ayush Jain <56622312+ayushjain123@users.noreply.github.com>
2021-05-23 10:24:19 +0530

    Update whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -2,8 +2,8 @@ import pywhatkit
 import csv
 from datetime import datetime
 time = datetime.now()
-with open("C://Users//wafa//Documents//contacts.csv","r") as file:
+with open("Path of csv File","r") as file:
     reader = csv.reader(file)
     for row in reader:
         time = datetime.now()
-        pywhatkit.sendwhatmsg(f"+91-{row[1]}","Welcome to TheMentorLive, This is Ayush Jain ",time.hour,time.minute+1,5)
+        pywhatkit.sendwhatmsg(f"+91-{row[1]}","Welcome to TheMentorLive ",time.hour,time.minute+1,5)
